8d7625c3837067d57040f2d32cc6dd995f04fd11
Raphael <paiva.raphaelt@gmail.com>
2020-07-07 00:49:00 +0200

    add state, props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import NavBar from './components/Navbar/Navbar';
 import Contains from './components/Contains/Contains';
 
-function App() {
+const App = () => {
+
+  const [displayState, setDisplayState] = useState({competition : false, match: false});
+
+  const getDisplayNavbar = (state) => {
+    setDisplayState(state);
+  }
+
   return (
     <div className="App">
-      <NavBar/>
-      <Contains />
+      <NavBar getDisplay={getDisplayNavbar}/>
+      <Contains display={displayState}/>
     </div>
   );
 }
