78f2a95111947995de8f2dbaf1ca8e6153bce485
Raphael <paiva.raphaelt@gmail.com>
2020-07-06 18:47:43 +0200

    create and add components contains

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import NavBar from './components/Navbar/Navbar';
+import Contains from './components/Contains/Contains';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Contains />
     </div>
   );
 }
---
+++ src/components/Contains/Contains.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contains = () => {
+    return (
+        <>
+            <h1>hello contains</h1>
+        </>
+    )
+}
+
+export default Contains;
