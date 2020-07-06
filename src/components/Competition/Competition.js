a0127e0a0819b7dec6b205550b0927f33671a207
Raphael <paiva.raphaelt@gmail.com>
2020-07-07 00:49:13 +0200

    create basic component

---
+++ src/components/Competition/Competition.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Competition = () => {
+    return (
+        <h1>Hello competition</h1>
+    )
+}
+
+export default Competition;
---
+++ src/components/Match/Match.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Match = () => {
+    return <h1>Helloe match</h1>
+}
+
+export default Match;
