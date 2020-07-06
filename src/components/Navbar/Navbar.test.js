613f5d2487f508cf4898e7632c8d2a0a0ab9d84c
Raphael <paiva.raphaelt@gmail.com>
2020-07-06 17:47:31 +0200

    render 4 li

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,9 +1,19 @@
-import React, { createElement } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import NavBar from './Navbar';
 
 it('renders component NavBar' , () => {
-    const div = createElement('div');
+    const div = document.createElement('div');
     ReactDOM.render(<NavBar />, div);
+});
+
+it('renders a 4 li', () => {
+    const { getByTestId } = render(<NavBar />) 
+    const numberElementChild = getByTestId('ulValue').childElementCount 
+    expect(numberElementChild).toBe(4)
+})
+
+it('each li get function', () => {
+    //const { getByTestId}
 })
